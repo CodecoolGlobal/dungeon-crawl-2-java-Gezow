927aec9f17c44fa5abe092008df89c1863fa2939
Gezow <kokenyesdanone@gmail.com>
2021-10-05 10:41:56 +0200

    blank cell constructor

--- src/main/java/com/codecool/dungeoncrawl/logic/Cell.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Cell.java
@@ -15,6 +15,10 @@ public class Cell implements Drawable {
         this.type = type;
     }
 
+    public Cell(CellType type){
+        this.type = type;
+    }
+
     public CellType getType() {
         return type;
     }
