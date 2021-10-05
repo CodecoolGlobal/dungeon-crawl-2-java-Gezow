b09b7f386c62944dab8181cae8336d373dc706fe
safarandras <safarandras00@gmail.com>
2021-10-05 12:04:42 +0200

    add: collectible subclasses

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/collectibles/Crystal.java
@@ -0,0 +1,8 @@
+package com.codecool.dungeoncrawl.logic.items.collectibles;
+
+public class Crystal extends Collectible{
+    @Override
+    public String getTileName() {
+        return "crystal";
+    }
+}
