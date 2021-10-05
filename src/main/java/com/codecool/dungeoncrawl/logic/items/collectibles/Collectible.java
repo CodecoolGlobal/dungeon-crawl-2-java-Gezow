d20d134b5c04cd196ad6d4e36c041a9dd4f69396
safarandras <safarandras00@gmail.com>
2021-10-05 15:46:27 +0200

    feat: collectible pickup

--- src/main/java/com/codecool/dungeoncrawl/logic/items/collectibles/Collectible.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/collectibles/Collectible.java
@@ -1,10 +1,17 @@
 package com.codecool.dungeoncrawl.logic.items.collectibles;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.logic.items.Item;
 
 public abstract class Collectible extends Item {
     protected int counter = 0;
+
+    @Override
+    public void pickUp(Player player){
+        player.getCollectibles().put(this.getTileName(), this);
+        counter = player.getCollectibles().size();
+    }
 
     public Collectible(Cell cell) {
         super(cell);
