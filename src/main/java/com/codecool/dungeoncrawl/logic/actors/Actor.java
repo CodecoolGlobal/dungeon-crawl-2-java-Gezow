6dbde9cb5487afa2656145e6232a89df11eda534
Gezow <kokenyesdanone@gmail.com>
2021-10-07 13:12:08 +0200

    actor enrage

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -4,6 +4,7 @@ import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
+import java.util.LinkedList;
 import java.util.Objects;
 
 public abstract class Actor implements Drawable {
@@ -36,6 +37,41 @@ public abstract class Actor implements Drawable {
             cell = nextCell;
         }
     }
+
+    public void enrage(int playerX, int playerY, int ragerX, int ragerY) {
+        LinkedList<Direction> rightDirections = new LinkedList<>();
+        for (Direction direction : Direction.values()
+        ) {
+            int dx = direction.getX();
+            int dy = direction.getY();
+            if (canMove(dx, dy) && (isCloserToTarget(playerX, playerY, ragerX, ragerY, dx, dy))) {
+                rightDirections.add(direction);
+            }
+        }
+        Direction randomRightDirection = rightDirections.get((int) (Math.random() * rightDirections.size()));
+        int dx = randomRightDirection.getX();
+        int dy = randomRightDirection.getY();
+        move(dx,dy);
+    }
+
+    private boolean isCloserToTarget(int targetX, int targetY, int actorX, int actorY, int nextX, int nextY) {
+        if (actorX < targetX){
+            if (nextX == 1){
+                return true;
+            }
+        }else{
+            if (nextX == -1){
+                return true;
+            }
+        }
+        if (actorY < targetY){
+            return nextY == 1;
+        }else{
+            return nextY == -1;
+        }
+    }
+
+    public abstract void autoMove(int frag, Player player);
 
     public int getMeleeDamage() {
         return meleeDamage;
