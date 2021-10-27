abb9d75762387f591ae4c613a2fcc4973fc485fc
Gezow <kokenyesdanone@gmail.com>
2021-10-27 17:06:51 +0200

    test cell

--- src/main/java/com/codecool/dungeoncrawl/logic/Cell.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Cell.java
@@ -44,7 +44,11 @@ public class Cell implements Drawable {
     }
 
     public Cell getNeighbor(int dx, int dy) {
-        return gameMap.getCell(x + dx, y + dy);
+        try {
+            return gameMap.getCell(x + dx, y + dy);
+        }catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            return null;
+        }
     }
 
     @Override
